c839a002962fb3ae19329bdc4e571f12a9e1a0e2
Matt Raible <matt.raible@okta.com>
2020-12-15 14:03:06 -0700

    Add Okta-hosted login example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
-import SecuredApp from './SecuredApp';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Private from './Private';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-6974382.okta.com/oauth2/default',
+  clientId: '0oa2i1po7LTnjRN6S5d6',
+  redirectUri: window.location.origin + '/callback'
+});
 
 function App() {
   return (
     <div className="App">
       <div className="page">
         <div className="content">
-          <Router>
-            <SecuredApp/>
-          </Router>
+          <BrowserRouter>
+            <Security oktaAuth={oktaAuth}>
+              <Header/>
+              <Route path='/' exact={true} component={Home}/>
+              <SecureRoute path='/private' exact={true} component={Private}/>
+              <Route path='/callback' component={LoginCallback}/>
+            </Security>
+          </BrowserRouter>
         </div>
       </div>
     </div>
