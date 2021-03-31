2e98544de53d529cba1b386e1ecad666c024951a
Matt Raible <matt.raible@okta.com>
2021-03-31 13:01:46 -0600

    Update Okta React and Sign-In Widget to v5

--- src/OktaSignInWidget.js
+++ src/OktaSignInWidget.js
@@ -1,67 +1,24 @@
 import React, { Component } from 'react';
-import { withOktaAuth } from '@okta/okta-react';
-import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSignIn from '@okta/okta-signin-widget';
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 
-class OktaSignInWidget extends Component {
+export default class OktaSignInWidget extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      sessionToken: null,
-      email: '',
-      password: ''
-    };
-
-    this.oktaAuth = new OktaAuth({
-      issuer: props.baseUrl,
-      clientId: '0oafhhdgdhMDvqaQW5d6',
-      pkce: true
+    this.wrapper = React.createRef();
+  }
+  componentDidMount() {
+    this.widget = new OktaSignIn({
+      baseUrl: this.props.baseUrl
     });
-
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.signIn = this.signIn.bind(this);
+    this.widget.renderEl({el: this.wrapper.current}, this.props.onSuccess, this.props.onError);
   }
 
-  handleEmailChange(event) {
-    this.setState({email: event.target.value});
-  }
-
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value});
-  }
-
-  async signIn(event) {
-    event.preventDefault();
-    const transaction = await this.oktaAuth.signIn({
-      username: this.state.email,
-      password: this.state.password
-    });
-
-    if (transaction.status === 'SUCCESS') {
-      this.props.oktaAuth.signInWithRedirect({sessionToken: transaction.sessionToken})
-    } else {
-      throw new Error('Could not sign in: ' + transaction.status);
-    }
+  componentWillUnmount() {
+    this.widget.remove();
   }
 
   render() {
-    return (
-      <form onSubmit={this.signIn} className="login-form">
-        <h2>Log In</h2>
-        <p>Please login to continue</p>
-        <label className="full-width-input">
-          Email
-          <input type="text" placeholder="Email" value={this.state.email} onChange={this.handleEmailChange} required />
-        </label>
-        <label className="full-width-input">
-          Password
-          <input type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange} required autoComplete="off" />
-        </label>
-        <button className="button">Login</button>
-      </form>
-    );
+    return <div ref={this.wrapper} />;
   }
 };
-
-export default withOktaAuth(OktaSignInWidget);
