c0daf35f85880957fdebb7690d93bf3d89c19763
Matt Raible <matt.raible@okta.com>
2021-03-31 13:06:57 -0600

    Update to use Okta React 5.0

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import Login from './Login';
 import Private from './Private';
 import { Route } from 'react-router-dom';
 import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
@@ -20,20 +19,14 @@ const App = () => {
   const restoreOriginalUri = async (_oktaAuth, originalUri) => {
     history.replace(toRelativeUrl(originalUri, window.location.origin));
   };
-  const onAuthRequired = function() {
-    history.push('/login')
-  }
 
   return (
     <div className="App">
       <div className="page">
         <div className="content">
-          <Security oktaAuth={oktaAuth}
-                    restoreOriginalUri={restoreOriginalUri}
-                    onAuthRequired={onAuthRequired}>
+          <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
             <Header/>
             <Route path='/' exact={true} component={Home}/>
-            <Route path='/login' exact={true} component={Login}/>
             <SecureRoute path='/private' exact={true} component={Private}/>
             <Route path='/callback' component={LoginCallback}/>
           </Security>
